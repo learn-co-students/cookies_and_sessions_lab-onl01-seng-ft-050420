a0c0a3dbc057bafc29bd533154a6885d524ff3d7
qu1xs <kevinnealmurphy@gmail.com>
2020-07-13 23:35:16 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+class ProductsController < ApplicationController
+    def index 
+    
+    end
+
+    def add
+        cart << params.require(:product)
+       
+        session[:cart] = cart
+
+        redirect_to root_path
+    end
+end
