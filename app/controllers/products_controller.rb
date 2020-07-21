eb5f486c77fd6df66852bae6dc6b60f018cc78a2
matthewlazewski <matthew.lazewski@gmail.com>
2020-07-21 14:42:19 -0500

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController 
+    def index 
+        @cart = cart 
+    end
+
+    def add 
+        cart << product_params
+        render "index"
+    end
+
+    private 
+    def product_params
+        params.require(:product)
+    end
+end
